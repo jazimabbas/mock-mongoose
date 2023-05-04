82f56ab0460cf1d233788db34ca690e6f8a9ab2b
Jazim Abbas <jazimabbas87@gmail.com>
2023-05-04 11:06:29 +0500

    Added helpers for mocking

---
+++ src/handle-mock.js
@@ -0,0 +1,75 @@
+module.exports = async function (cb) {
+  const {
+    op,
+    model: { modelName },
+    _mongooseOptions = {},
+  } = this;
+  const Model = mongoose.model(modelName);
+
+  let mock =
+    mockingoose.__mocks[modelName] && mockingoose.__mocks[modelName][op];
+
+  let err = null;
+
+  if (mock instanceof Error) {
+    err = mock;
+  }
+
+  if (typeof mock === 'function') {
+    mock = await mock(this);
+  }
+
+  if (!mock && op === 'save') {
+    mock = this;
+  }
+
+  if (!mock && op === '$save') {
+    mock = this;
+  }
+
+  if (
+    mock &&
+    !(mock instanceof Model) &&
+    ![
+      'remove',
+      'deleteOne',
+      'deleteMany',
+      'update',
+      'updateOne',
+      'updateMany',
+      'count',
+      'countDocuments',
+      'estimatedDocumentCount',
+      'distinct',
+    ].includes(op)
+  ) {
+    mock = Array.isArray(mock)
+      ? mock.map((item) => new Model(item))
+      : new Model(mock);
+
+    if (op === 'insertMany') {
+      if (!Array.isArray(mock)) mock = [mock];
+
+      for (const doc of mock) {
+        const e = doc.validateSync();
+        if (e) throw e;
+      }
+    }
+
+    if (_mongooseOptions.lean || _mongooseOptions.rawResult) {
+      mock = Array.isArray(mock)
+        ? mock.map((item) => item.toObject())
+        : mock.toObject();
+    }
+  }
+
+  if (cb) {
+    return cb(err, mock);
+  }
+
+  if (err) {
+    throw err;
+  }
+
+  return mock;
+};
